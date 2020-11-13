227bdccdee5668de24e4f9bff4dcd3aed54bb2de
Dreptu <james.martin@asdnh.org>
2020-11-12 20:34:38 -0500

    Finished writing code for encoding the base test, still needs testing

--- beaufort.cpp
+++ beaufort.cpp
@@ -3,3 +3,30 @@
 #include "beaufort.h"
 using namespace std;
 
+string Beaufort::encode(string baseText, string key = "") {
+    string output = "";
+    string table = "abcdefghijklmnopqrstuvwxyz";
+    for(int i=0; i<baseText.size(); i++) {
+        char charText = baseText[i];
+        char charKey = key[i % key.size()];
+        int tempInt = 0;
+        for (int j=0; j<26; j++) {
+
+            if(table[j] == charText) {
+                tempInt = j;
+            }
+
+            int shift = 0;
+            for(int k=0; k<tempInt; k++) {
+                int letter = k % 26;
+                if (table[letter] == charKey) {
+                    break;
+                }
+                shift++;
+            }
+            output += table[shift];
+        }
+
+    }
+    return output;
+}
--- beaufort.h
+++ beaufort.h
@@ -8,5 +8,5 @@ class Beaufort : public Cipher{
     virtual std::string encode(std::string baseText, std::string key = "") override;
     virtual std::string decode(std::string cipherText, std::string key = "") override;
     virtual bool isValidKey(std::string key) override;
-    virtual void start() override;
+    virtual void start();
 }
