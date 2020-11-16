681b176eb0c541f9a1edd73aaea906fe60dff8f9
Dreptu <james.martin@asdnh.org>
2020-11-16 18:50:29 -0500

    Made the decode function

--- beaufort.cpp
+++ beaufort.cpp
@@ -30,3 +30,31 @@ string Beaufort::encode(string baseText, string key = "") {
     }
     return output;
 }
+
+string Beaufort::code(string cipherText, string key = "") {
+    string output = "";
+    string table = "abcdefghijklmnopqrstuvwxyz";
+    for(int i=0; i<cipherText.size(); i++) {
+        char charText = cipherText[i];
+        char charKey = key[i % key.size()];
+        int tempInt = 0;
+        for (int j=0; j<26; j++) {
+
+            if(table[j] == charText) {
+                tempInt = j;
+            }
+
+            int shift = 0;
+            for(int k=0; k<tempInt; k++) {
+                int letter = k % 26;
+                if (table[letter] == charKey) {
+                    break;
+                }
+                shift++;
+            }
+            output += table[shift];
+        }
+
+    }
+    return output;
+}
