e01852ca713958b5cdfeb254d885bd32e559cc7b
Griffin Speidel <32367669+ZedLeppin@users.noreply.github.com>
2020-11-23 19:32:38 -0500

    foop works for all n values

--- foop.cpp
+++ foop.cpp
@@ -5,22 +5,36 @@
 using namespace std;
 
 Foop::Foop(){
-    m_keyPrompt = "No key required";
+    m_keyPrompt = "Please enter an integer >= 0 and < the length of the text: ";
 }
 
 string Foop::encode(string baseText, string key){
+    int n = stoi(key);
     string result = baseText;
-    for(int i = 0; i < baseText.length()/2; i++){
-        result[2*i] = baseText[2*i+1];
-        result[2*i+1] = baseText[2*i];
+    int len = baseText.length();
+    bool hasTouched[len];
+    fill_n(hasTouched, len, false);
+    for(int i = 0; i + n < len; i++){
+        if(!hasTouched[i] && !hasTouched[i+n]){
+            result[i] = baseText[i+n];
+            result[i+n] = baseText[i];
+            hasTouched[i] = true;
+            hasTouched[i+n] = true;
+        }
     }
     return result;
 }
 
 string Foop::decode(string cipherText, string key){
-    return encode(cipherText, "");
+    return encode(cipherText, key);
 }
 
 bool Foop::isValidKey(string key){
+    /*for(char c : key){
+        if(!isdigit(c)){
+            return false;
+        }
+    }
+    return stoi(key) >= 0 && stoi(key) < key.length();*/
     return true;
 }
