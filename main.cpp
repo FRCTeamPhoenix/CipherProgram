73264a3f3fd879c057808106c91e1bd0044fc580
Leo <leo.bunkartas@asdnh.org>
2020-11-23 19:50:11 -0500

    Added encode and decode feature

--- main.cpp
+++ main.cpp
@@ -3,12 +3,15 @@
 
 #include "cipherprogram.h"
 #include "caesercipher.h"
+#include "runningkeycipher.h"
 
 int main(){
     CipherProgram program;
     CaeserCipher caesar;
+    RunningKeyCipher running;
 
     program.addCipher("caesar", &caesar);
+    program.addCipher("running", &running);
     program.run();
     
     return 0;
--- runningkeycipher.cpp
+++ runningkeycipher.cpp
@@ -2,10 +2,11 @@
 
 #include <iostream>
 #include <algorithm>
+#include <string>
 #include <ctype.h>
 
 RunningKeyCipher::RunningKeyCipher(){
-    m_keyPrompt = "Enter text with no numbers. Equal to or greater than message.";
+    m_keyPrompt = "Enter text with no numbers. Equal to the message. (Case senseitive)";
 }
 
 const char LETTER[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
@@ -20,11 +21,29 @@ int getValue(char c){
 }
 
 std::string RunningKeyCipher::encode(std::string baseText, std::string key){
+    std::string result = "";
+     for(int i = 0; i < baseText.length(); i++){
+            int totalVal = getValue(key[i]) + getValue(baseText[i]);
+            while(totalVal > 25){
+                totalVal -= 26;
+            }
+            result += LETTER[totalVal];
+        }
+        //cout << stackedKey << endl << baseText << endl;
+        return result;
+    }
 
-}
 
 std::string RunningKeyCipher::decode(std::string cipherText, std::string key){
-
+   std::string result = "";
+         for(int i = 0; i < cipherText.length(); i++){
+            int index = getValue(cipherText[i]) - getValue(key[i]);
+            while(index < 0){
+                index += 26;
+            }
+            result += LETTER[index];
+        }
+        return result;
 }
 bool RunningKeyCipher::isValidKey(std::string key){
     for (int i = 0; i < key.length(); i++){
